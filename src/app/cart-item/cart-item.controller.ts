bc3a1b12b8e3ae0e5eca862559e36e8b6783f009
MetinSoyalp <metinsoyalp344@gmail.com>
2024-12-13 02:15:22 +0300

    Added addItemToCart endpoint and its unit tests

--- src/app/cart-item/cart-item.controller.ts
+++ src/app/cart-item/cart-item.controller.ts
@@ -1,10 +1,14 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Param, Patch, Query } from "@nestjs/common";
 import { CartItemService } from "./cart-item.service";
 
 @Controller('api/v1/cart-item')
 export class CartItemController {
     constructor(private readonly cartItemService: CartItemService) {}
 
-    //Add api endpoints
+    @Patch('add-item/:bookId')
+    async addItemToCart( @Param('bookId') bookId: number, @Query('u_id') userId: number = 0 ) {
+        const result = this.cartItemService.addItemToCart(bookId, userId);
+        return result;
+    }
 
 }
