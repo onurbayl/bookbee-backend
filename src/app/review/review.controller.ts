124a4c97c68060147dcadc4c41f3bd7450b5a343
Giray Yıldırım <147543664+yildirimg21@users.noreply.github.com>
2024-12-25 11:58:45 +0300

    Delete a Review
    Endpoint for deleting an existing review.

--- src/app/review/review.controller.ts
+++ src/app/review/review.controller.ts
@@ -1,10 +1,17 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Param, UseGuards, Request, Get, Post, Delete } from "@nestjs/common";
 import { ReviewService } from "./review.service";
+import { AuthGuard } from "src/guards/auth.guard";
 
 @Controller('api/v1/review')
 export class ReviewController {
     constructor(private readonly reviewService: ReviewService) {}
 
-    //Add api endpoints
+    @Delete('delete-review/:bookId/:userId')
+    @UseGuards(AuthGuard)
+    async DeleteReview(@Param('bookId') bookId: number, @Param('userId') userId: number, @Request() req){
+        const uId = req.user.uid;
+        const result = this.reviewService.deleteReview(bookId, userId);
+        return result;
+    }
 
 }
--- src/app/review/review.service.ts
+++ src/app/review/review.service.ts
@@ -1,14 +1,47 @@
 import { Injectable } from "@nestjs/common";
 import { ReviewRepository } from "./review.repository";
 import { InjectRepository } from "@nestjs/typeorm";
+import { BookRepository } from "../book/book.repository";
+import { BookNotFoundException } from "../book/exceptions/book-not-found.exception";
+import { UserRepository } from "../user/user.repository";
+import { UserNotFoundException } from "../user/exceptions/user-not-found.exception";
+import { Review } from "./review.entity";
+import { ReviewNotFoundException } from "./exceptions/review-not-found-exception";
 
 @Injectable()
 export class ReviewService {
     constructor( //Injects repositories that you want to use
         @InjectRepository(ReviewRepository)
         private readonly reviewRepository: ReviewRepository,
+
+        @InjectRepository(BookRepository)
+        private readonly bookRepository: BookRepository,
+
+        @InjectRepository(UserRepository)
+        private readonly userRepository: UserRepository
     ) {}
 
-    //Add service methods
+    async deleteReview(bookId: number, userId: number){
+
+        const user = await this.userRepository.findById(userId);
+        if(user == null){
+            UserNotFoundException.byId(userId);
+        }
+
+        const book = await this.bookRepository.findById(bookId);
+        if(book == null){
+            BookNotFoundException.byId(bookId);
+        }
+
+        let review = await this.reviewRepository.findByBookAndUser(bookId, user.id);
+        if(review == null){
+            ReviewNotFoundException.byBookAndUser(bookId, user.id);
+        }
+
+        await this.reviewRepository.delete(review);
+
+        return { message: 'This review by the user ' + userId + ' was removed.' }
+        
+    }
 
 }
