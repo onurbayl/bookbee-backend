55361824fe6c6988bb3b3ceb6645cc6aea8a3e17
MetinSoyalp <metinsoyalp344@gmail.com>
2024-12-27 02:08:36 +0300

    Port number changed to 3000

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
     });
   }
 
-  const port = process.env.PORT || 10000;
+  const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
   console.log(`Application is running on http://localhost:${port}`);
 }
