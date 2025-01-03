1dd1fab5822ecf5bcdc9910fe26b00431126f4bc
Hakan Duran <125091245+Hakan-Duran@users.noreply.github.com>
2025-01-03 07:04:13 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -13,12 +13,7 @@ echo "Installing Docker Compose..."
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-# Step 4: Move docker-compose.yml and dataset.sql to the upper directory
-echo "Moving docker-compose.yml and dataset.sql to the upper directory..."
-mv docker-compose.yml ../
-mv development_bookbee_dataset.sql ../
-
-# Step 5: Notify the user to create .env files
+# Step 4: Notify the user to create .env files
 echo "You need to create .env files for backend and frontend code!"
 
 # Script completed
